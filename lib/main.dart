3a92182baf5e7b9f1904c4f7e982c815de221dc4
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-07-07 11:58:24 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_keep_file/pages/home_page.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home: const HomePage(title: 'Flutter Keep File'),
+      );
+}
